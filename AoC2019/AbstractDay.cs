0b7818d687a2bfbdb131fd9c4750c3f6bd260ac6
Delthii <32238382+Delthii@users.noreply.github.com>
2019-10-05 21:02:51 +0200

    Removed unused usings

--- AoC2019/AbstractDay.cs
+++ AoC2019/AbstractDay.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AoC2019
