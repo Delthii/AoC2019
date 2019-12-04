395a3f2f8c41faca52463725aa8d58dba656915b
Peter Malmquist <pma@combination.se>
2019-12-04 07:27:32 +0100

    day4

--- AoC2019/Day19_4.cs
+++ AoC2019/Day19_4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AoC2019
@@ -7,18 +8,62 @@ namespace AoC2019
     {
         public Day19_4() : base(4, 2019)
         {
-            string[] Input = File.ReadAllLines("..\\..\\in.txt");
-
         }
 
-        public override string PartA()
+        public override string PartA() => Core(AdjA);
+
+        public override string PartB() => Core(AdjB);
+
+        private static string Core(Func<char[], int, bool, bool> adjFunc)
         {
-            throw new NotImplementedException();
+            var pws = 0;
+            for (var i = 402328; i < 864247; i++)
+            {
+                var pw = i.ToString().ToCharArray();
+                var adj = false;
+                var inc = true;
+
+                for (int c = 0; c < pw.Length - 1; c++)
+                {
+                    adj = adjFunc(pw, c, adj);
+                    if (pw[c + 1] < pw[c])
+                    {
+                        inc = false;
+                        break;
+                    }
+                }
+
+                if (adj && inc) pws++;
+            }
+
+            return pws.ToString();
         }
 
-        public override string PartB()
+        private bool AdjA(char[] pw, int c, bool adj)
         {
-            throw new NotImplementedException();
+            if (pw[c] == pw[c + 1]) adj = true;
+
+            return adj;
         }
+
+        private bool AdjB(char[] pw, int c, bool adj)
+        {
+            if (pw[c] == pw[c + 1])
+            {
+                if (c > 0)
+                {
+                    if (pw[c - 1] == pw[c]) return adj;
+                }
+                if (c < pw.Length - 2)
+                {
+                    if (pw[c + 2] == pw[c]) return adj;
+                }
+
+                return true;
+            }
+
+            return adj;
+        }
+
     }
 }
