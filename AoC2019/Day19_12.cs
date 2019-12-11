4453fd3bfbc9f1c72434165c251358b384bdecf5
Delthii <32238382+Delthii@users.noreply.github.com>
2019-12-11 21:44:43 +0100

    day12 prep

---
+++ AoC2019/Day19_12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2019
+{
+    class Day19_12 : AbstractDay
+    {
+        public Day19_12() : base (12,2019)
+        {
+            
+        }
+
+        public override string PartA()
+        {
+            var input = Lines.ToArray();
+
+            return "";
+        }
+
+        public override string PartB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
